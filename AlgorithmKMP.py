aee17bb08e7bb384a1ebf899065a7755f4ac8410
ezdimitry <137773973+ezdimitry@users.noreply.github.com>
2024-08-09 14:46:45 +0300

    add KMP algorithm

---
+++ AlgorithmKMP.py
@@ -0,0 +1,41 @@
+"Алгоритм на поиск образа в строке"
+
+t = "лилила"
+
+p = [0]*len(t)
+j = 0
+i = 1
+while i < len(t):
+    if t[j] == t[i]:
+        p[i] = j+1
+        i += 1
+        j += 1
+    else:
+        if j == 0:
+            p[i] = 0
+            i += 1
+        else:
+            j = p[j-1]
+print("Массив p:", p)
+
+
+a = "лилилось лилилось"
+m = len(t)
+n = len(a)
+
+i = 0
+j = 0
+while i < n:
+    if a[i] == t[j]:
+        i += 1
+        j += 1
+        if j == m:
+            print("Образ найден")
+            break
+    else:
+        if j > 0:
+            j = p[j-1]
+        else:
+            i += 1
+if i == n:
+    print("Образ не найден")
